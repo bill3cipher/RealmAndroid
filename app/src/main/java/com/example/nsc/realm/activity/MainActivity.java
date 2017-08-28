47ed58ac4bb66452a14285d4ea8bd18f4590421f
chakhrits <chakhritz@gmail.com>
2017-08-28 11:13:56 +0700

    complete

--- app/src/main/java/com/example/nsc/realm/activity/MainActivity.java
+++ app/src/main/java/com/example/nsc/realm/activity/MainActivity.java
@@ -2,14 +2,11 @@ package com.example.nsc.realm.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.nsc.realm.Model.Student;
@@ -19,7 +16,6 @@ import com.example.nsc.realm.adapter.MyAdapter;
 import java.util.ArrayList;
 
 import io.realm.Realm;
-import io.realm.RealmQuery;
 import io.realm.RealmResults;
 
 
